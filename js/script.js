407efe53bb35457c98bf48fecdcaad322be9bc2c
Asierlm <asier.lopmar@educa.jcyl.es>
2025-02-23 18:37:30 +0100

    Añadido Boton en el html y css Creado codigo javascript para los botones

--- js/script.js
+++ js/script.js
@@ -1,1 +1,18 @@
-  
+
+// Esperar a que la página cargue completamente
+document.addEventListener("DOMContentLoaded", function () {
+    // Seleccionamos el botón por su ID
+    let BotonTortilla = document.getElementById("BotonTortilla");
+    // Segundo botón
+    let BotonEnsalada = document.getElementById("BotonEnsalada");
+
+    // Agregamos el evento de clic para redirigir a la página de Tortilla
+    BotonTortilla.addEventListener("click", function () {
+        window.open("https://es.wikipedia.org/wiki/Tortilla_de_patatas", "_blank");
+    });
+
+    // Agregamos el evento de clic para redirigir a la página de Ensalada César
+    BotonEnsalada.addEventListener("click", function () {
+        window.open("https://es.wikipedia.org/wiki/Ensalada_C%C3%A9sar", "_blank");
+    });
+});
